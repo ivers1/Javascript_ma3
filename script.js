7728cbb53ce7a7ee843dec4e3ae474b6eca6c4c2
ivers1 <39339171+ivers1@users.noreply.github.com>
2018-10-21 22:16:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//Console.log out elements in JSON file
+//Use RegEx to validate form
+function required()
+{
+var empt = document.form.name1.name2;
+if (empt === "")
+{
+alert("Please input a Value");
+return false;
+}
+else 
+{
+alert("Please input first and last name");
+return true; 
+}
+}
