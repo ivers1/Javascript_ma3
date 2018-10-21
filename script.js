50d2d0d7c1a21461fc1ef1c781716f0f6113e5f4
ivers1 <39339171+ivers1@users.noreply.github.com>
2018-10-21 22:19:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-//Use RegEx to validate form
-function required()
-{
-var empt = document.form.name1.name2;
-if (empt === "")
-{
-alert("Please input a Value");
-return false;
-}
-else 
-{
-alert("Please input first and last name");
-return true; 
-}
-}
+//Console.log out elements in JSON file
