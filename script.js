653f9eed35dd6eef69448530793bf72fa9b20ca0
ivers1 <39339171+ivers1@users.noreply.github.com>
2018-10-21 23:15:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //Console.log out elements in JSON file
+$(window).on("load", function() {
+      
+info = [{"id" : "12312412312", "name" : "Ecuaciones Diferenciales", "url" : "/video/math/edo/12312412312s", "author" : "", "name_author" : "Alejandro Morales", "url_a" : "/author/alejandro-morales", "type" : "master" }];
+
+(function() {
+    var myinfo = JSON.parse(info);
+    console.log(myinfo[0].id);
+    console.log(myinfo[0].name);
+    console.log(myinfo[0].url);
+    console.log(myinfo[0].author);
+    console.log(myinfo[0].name_author);
+    console.log(myinfo[0].url_a);
+    console.log(myinfo[0].type);
+})()
+}
+
+
+
+
+
+
+
+
+
